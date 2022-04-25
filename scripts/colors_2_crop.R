4a23108ab0128dc6c610c204e4ec48d1610b1331
alexandralalor <allielalor@gmail.com>
2022-04-25 16:11:46 -0700

    colors script!!

--- scripts/colors_2_crop.R
+++ scripts/colors_2_crop.R
@@ -12,6 +12,7 @@ library(magick) #for cropping
 
 
 #create objects for old and new file paths
+#file path for local computer
 my_path_seg <- "C:/Users/allie/Desktop/UofA/HW project/heatwave_analysis/Phase 1 Photos/November 5 2021/Segmented/"
 my_path_crop <- "C:/Users/allie/Desktop/UofA/HW project/heatwave_analysis/Phase 1 Photos/November 5 2021/Crop/"
 
@@ -19,14 +20,23 @@ file_names_old_jpg <- list.files(my_path_seg)
 file_names_old <- file_path_sans_ext(file_names_old_jpg)
 file_names_crop <- paste0(file_names_old,"_crop",".jpg")
 
+#file path for external hard drive
+#adjust file path for each date
+my_path_seg <- "E:/Phase 1 Data/Phase 1 Photos/August 26 2021/Segmented/"
+my_path_crop <- "E:/Phase 1 Data/Phase 1 Photos/August 26 2021/Crop/"
+
+file_names_old_jpg <- list.files(my_path_seg)
+file_names_old <- file_path_sans_ext(file_names_old_jpg)
+file_names_crop <- paste0(file_names_old,"_crop",".jpg")
+
 
 #for loop to crop photo, rename it, and save it to "Crop" folder
-for (i in 1:length(file_names_old)) {
-  file_name <- file_names_old[i]
+for (i in 1:length(file_names_old_jpg)) {
+  file_name <- file_names_old_jpg[i]
   file_names_new <- file_names_crop[i]
   pic <- paste0(my_path_seg, file_name)
   pic1 <- image_read(pic)
-  pic1_crop <- image_crop(pic1, geometry = "0x3550")
+  pic1_crop <- image_crop(pic1, geometry = "0x3450")
   my_path_final <- paste0(my_path_crop, file_names_new)
   image_write(pic1_crop, path = my_path_final)
 }
