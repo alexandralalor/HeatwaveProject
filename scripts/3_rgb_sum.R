15759ed81d5b40bbf6d47b066fe3a842f22eb3df
alexandralalor <allielalor@gmail.com>
2022-06-11 15:26:22 -0700

    update script for large file size

--- scripts/3_rgb_sum.R
+++ scripts/3_rgb_sum.R
@@ -2,7 +2,7 @@
 #To summarize data from all pixels. This means creating bins and grouping colors, as well as cleaning up some columns
 #allielalor@email.arizona.edu
 #First created: 2022-05-02
-#Last updated: 2022-06-08
+#Last updated: 2022-06-11
 
 #load libraries
 library(tidyverse)
@@ -16,7 +16,7 @@ library(ggtern) #for rbg2hex
 ################################################################################
 
 #read in files
-tree_rgb <- read_csv("E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb/tree_rgb_August_26_2021.csv")
+# tree_rgb <- read_csv("E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb/tree_rgb_August_26_2021.csv")
 # tree_rgb <- read_csv("E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb/tree_rgb_September_02_2021.csv")
 # tree_rgb <- read_csv("E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb/tree_rgb_September_09_2021.csv")
 # tree_rgb <- read_csv("E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb/tree_rgb_September_16_2021.csv")
@@ -66,10 +66,18 @@ tree_rgb <- read_csv("E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb/tree_rgb_A
 #reducing the data frame
 
 #bining
-glimpse(tree_rgb)
+tree_rgb %>% 
+  summarize(species = unique(Species))
+
+tree_rgb_chamber1 <- tree_rgb %>% 
+  filter(Species == "PIPO", Species == "PIED")
+tree_rgb_chamber2 <- tree_rgb %>% 
+  filter(Species == "PSME", Species == "PIFL")
+tree_rgb_chamber3 <- tree_rgb %>% 
+  filter(Species == "PIEN")
 
 #bin red
-tree_rgb <- tree_rgb %>% 
+tree_rgb_PIFL <- tree_rgb_PIFL %>% 
   mutate(red_class = ifelse(red <= 10, "1-10", 
                             ifelse(red <= 20, "11-20", 
                                    ifelse(red <= 30, "21-30",
@@ -97,63 +105,65 @@ tree_rgb <- tree_rgb %>%
                                                                                                                                                                                              ifelse(red <= 250, "241-250", "250-255"))))))))))))))))))))))))))
 ##############################################################################################################################################################################################################################################################
 #bin green
-tree_rgb <- tree_rgb %>% 
+tree_rgb_PIFL <- tree_rgb_PIFL %>% 
   mutate(green_class = ifelse(green <= 10, "1-10", 
-                            ifelse(green <= 20, "11-20", 
-                                   ifelse(green <= 30, "21-30",
-                                          ifelse(green <= 40, "31-40",
-                                                 ifelse(green <= 50, "41-50",
-                                                        ifelse(green <= 60, "51-60",
-                                                               ifelse(green <= 70, "61-70",
-                                                                      ifelse(green <= 80, "71-80",
-                                                                             ifelse(green <= 90, "81-90",
-                                                                                    ifelse(green <= 100, "91-100",
-                                                                                           ifelse(green <= 110, "101-110",
-                                                                                                  ifelse(green <= 120, "111-120",
-                                                                                                         ifelse(green <= 130, "121-130",
-                                                                                                                ifelse(green <= 140, "131-140",
-                                                                                                                       ifelse(green <= 150, "141-150",
-                                                                                                                              ifelse(green <= 160, "151-160",
-                                                                                                                                     ifelse(green <= 170, "161-170",
-                                                                                                                                            ifelse(green <= 180, "171-180",
-                                                                                                                                                   ifelse(green <= 190, "181-190",
-                                                                                                                                                          ifelse(green <= 200, "191-200",
-                                                                                                                                                                 ifelse(green <= 210, "201-210",
-                                                                                                                                                                        ifelse(green <= 220, "211-220",
-                                                                                                                                                                               ifelse(green <= 230, "221-230",
-                                                                                                                                                                                      ifelse(green <= 240, "231-240",
-                                                                                                                                                                                             ifelse(green <= 250, "241-250", "250-255"))))))))))))))))))))))))))
+                              ifelse(green <= 20, "11-20", 
+                                     ifelse(green <= 30, "21-30",
+                                            ifelse(green <= 40, "31-40",
+                                                   ifelse(green <= 50, "41-50",
+                                                          ifelse(green <= 60, "51-60",
+                                                                 ifelse(green <= 70, "61-70",
+                                                                        ifelse(green <= 80, "71-80",
+                                                                               ifelse(green <= 90, "81-90",
+                                                                                      ifelse(green <= 100, "91-100",
+                                                                                             ifelse(green <= 110, "101-110",
+                                                                                                    ifelse(green <= 120, "111-120",
+                                                                                                           ifelse(green <= 130, "121-130",
+                                                                                                                  ifelse(green <= 140, "131-140",
+                                                                                                                         ifelse(green <= 150, "141-150",
+                                                                                                                                ifelse(green <= 160, "151-160",
+                                                                                                                                       ifelse(green <= 170, "161-170",
+                                                                                                                                              ifelse(green <= 180, "171-180",
+                                                                                                                                                     ifelse(green <= 190, "181-190",
+                                                                                                                                                            ifelse(green <= 200, "191-200",
+                                                                                                                                                                   ifelse(green <= 210, "201-210",
+                                                                                                                                                                          ifelse(green <= 220, "211-220",
+                                                                                                                                                                                 ifelse(green <= 230, "221-230",
+                                                                                                                                                                                        ifelse(green <= 240, "231-240",
+                                                                                                                                                                                               ifelse(green <= 250, "241-250", "250-255"))))))))))))))))))))))))))
 ################################################################################################################################################################################################################################################################
 #bin blue
-tree_rgb <- tree_rgb %>% 
+tree_rgb_PIFL <- tree_rgb_PIFL %>% 
   mutate(blue_class = ifelse(blue <= 10, "1-10", 
-                            ifelse(blue <= 20, "11-20", 
-                                   ifelse(blue <= 30, "21-30",
-                                          ifelse(blue <= 40, "31-40",
-                                                 ifelse(blue <= 50, "41-50",
-                                                        ifelse(blue <= 60, "51-60",
-                                                               ifelse(blue <= 70, "61-70",
-                                                                      ifelse(blue <= 80, "71-80",
-                                                                             ifelse(blue <= 90, "81-90",
-                                                                                    ifelse(blue <= 100, "91-100",
-                                                                                           ifelse(blue <= 110, "101-110",
-                                                                                                  ifelse(blue <= 120, "111-120",
-                                                                                                         ifelse(blue <= 130, "121-130",
-                                                                                                                ifelse(blue <= 140, "131-140",
-                                                                                                                       ifelse(blue <= 150, "141-150",
-                                                                                                                              ifelse(blue <= 160, "151-160",
-                                                                                                                                     ifelse(blue <= 170, "161-170",
-                                                                                                                                            ifelse(blue <= 180, "171-180",
-                                                                                                                                                   ifelse(blue <= 190, "181-190",
-                                                                                                                                                          ifelse(blue <= 200, "191-200",
-                                                                                                                                                                 ifelse(blue <= 210, "201-210",
-                                                                                                                                                                        ifelse(blue <= 220, "211-220",
-                                                                                                                                                                               ifelse(blue <= 230, "221-230",
-                                                                                                                                                                                      ifelse(blue <= 240, "231-240",
-                                                                                                                                                                                             ifelse(blue <= 250, "241-250", "250-255"))))))))))))))))))))))))))
+                             ifelse(blue <= 20, "11-20", 
+                                    ifelse(blue <= 30, "21-30",
+                                           ifelse(blue <= 40, "31-40",
+                                                  ifelse(blue <= 50, "41-50",
+                                                         ifelse(blue <= 60, "51-60",
+                                                                ifelse(blue <= 70, "61-70",
+                                                                       ifelse(blue <= 80, "71-80",
+                                                                              ifelse(blue <= 90, "81-90",
+                                                                                     ifelse(blue <= 100, "91-100",
+                                                                                            ifelse(blue <= 110, "101-110",
+                                                                                                   ifelse(blue <= 120, "111-120",
+                                                                                                          ifelse(blue <= 130, "121-130",
+                                                                                                                 ifelse(blue <= 140, "131-140",
+                                                                                                                        ifelse(blue <= 150, "141-150",
+                                                                                                                               ifelse(blue <= 160, "151-160",
+                                                                                                                                      ifelse(blue <= 170, "161-170",
+                                                                                                                                             ifelse(blue <= 180, "171-180",
+                                                                                                                                                    ifelse(blue <= 190, "181-190",
+                                                                                                                                                           ifelse(blue <= 200, "191-200",
+                                                                                                                                                                  ifelse(blue <= 210, "201-210",
+                                                                                                                                                                         ifelse(blue <= 220, "211-220",
+                                                                                                                                                                                ifelse(blue <= 230, "221-230",
+                                                                                                                                                                                       ifelse(blue <= 240, "231-240",
+                                                                                                                                                                                              ifelse(blue <= 250, "241-250", "250-255"))))))))))))))))))))))))))
 ###############################################################################################################################################################################################################################################################
+glimpse(tree_rgb_sum)
+
 #summarize by bins
-tree_rgb_sum <- tree_rgb %>% 
+tree_rgb_sum_PIFL <- tree_rgb_PIFL %>% 
   group_by(Week, Date, Species, SpeciesID, Treatment_temp, Treatment_water, red_class, green_class, blue_class) %>% 
   summarize(red = round(mean(red)),
             green = round(mean(green)),
@@ -163,21 +173,24 @@ tree_rgb_sum <- tree_rgb %>%
 
 #fill in summary df
 #add hex codes to summary df
-hex <- as.data.frame(rgb2hex(r = tree_rgb_sum$red, 
-                             g = tree_rgb_sum$green, 
-                             b = tree_rgb_sum$blue))
+hex <- as.data.frame(rgb2hex(r = tree_rgb_sum_PIFL$red, 
+                             g = tree_rgb_sum_PIFL$green, 
+                             b = tree_rgb_sum_PIFL$blue))
 colnames(hex) <- "col_hex"
-tree_rgb_sum <- cbind(tree_rgb_sum, hex)
+tree_rgb_sum_PIFL <- cbind(tree_rgb_sum_PIFL, hex)
 #reorder columns
-tree_rgb_sum <- tree_rgb_sum[, c(1,2,3,4,5,6,7,8,9,10,11,12,14,13)]
+tree_rgb_sum_PIFL <- tree_rgb_sum_PIFL[, c(1,2,3,4,5,6,7,8,9,10,11,12,14,13)]
 #calculate total # pixels and percent of each color, add to summary df
-tree_rgb_sum <- tree_rgb_sum %>% 
+tree_rgb_sum_PIFL <- tree_rgb_sum_PIFL %>% 
   group_by(Week, Date, Species, SpeciesID, Treatment_temp, Treatment_water) %>% 
   mutate(col_total = sum(col_freq)) %>% 
   mutate(col_share = round(100*(col_freq/col_total), digits = 1))
 
+
+tree_rgb_sum <- rbind(tree_rgb_sum_chamber1, tree_rgb_sum_chamber2, tree_rgb_sum_chamber3)
+
+
 #Remember to change date when saving!
 #write csv
-write.csv(tree_rgb_sum, "E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb_sum/tree_rgb_sum_August_26_2021.csv", quote=FALSE, row.names = FALSE)
-
+write.csv(tree_rgb_sum, "E:/Data/Phase1_Data/Phase1_Photos_Data/tree_rgb_sum/tree_rgb_sum_September_16_2021.csv", quote=FALSE, row.names = FALSE)
 
